ec8a88f922d5217793366a531c332965bb3b51bd
BuildTools <tom@scr1ptme.com>
2022-04-05 14:00:40 +0200

    Getter & Setter for bonus

--- abstract/Mammifere.ts
+++ abstract/Mammifere.ts
@@ -1,9 +1,9 @@
 import Animal from "../interface/Animal";
 
 export default abstract class Mammifere implements Animal {
-    _dateNaissance: Date;
-    _nom: string;
-    _poids: number;
+    private _dateNaissance: Date;
+    private _nom: string;
+    private _poids: number;
 
 
     constructor(nom: string, poids: number, dateNaissance: Date) {
@@ -16,4 +16,29 @@ export default abstract class Mammifere implements Animal {
      * Affiche les informations du mammifères
      */
     display() {}
+
+
+    get dateNaissance(): Date {
+        return this._dateNaissance;
+    }
+
+    set dateNaissance(value: Date) {
+        this._dateNaissance = value;
+    }
+
+    get nom(): string {
+        return this._nom;
+    }
+
+    set nom(value: string) {
+        this._nom = value;
+    }
+
+    get poids(): number {
+        return this._poids;
+    }
+
+    set poids(value: number) {
+        this._poids = value;
+    }
 }
